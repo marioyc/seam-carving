a1511e353d37d1bca7940de2b89d68c5051525af
marioyc <ycmario@gmail.com>
2015-02-21 04:03:55 +0100

    find seams

---
+++ seam-carving.cpp
@@ -0,0 +1,166 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+Mat calculate_energy(Mat I){
+	Mat Ix,Iy;
+
+	Sobel(I,Ix,CV_32F,1,0);
+	convertScaleAbs(Ix,Ix);
+
+	Sobel(I,Iy,CV_32F,0,1);
+	convertScaleAbs(Iy,Iy);
+
+	Mat energy;
+	addWeighted(Ix, 0.5, Iy, 0.5, 0, energy);
+
+	return energy;
+}
+
+Mat calculate_energy2(Mat I){
+	int Y = I.rows,X = I.cols;
+	Mat energy = Mat(Y, X, CV_32S);
+
+	for(int x = 0;x < X;++x){
+		for(int y = 0;y < Y;++y){
+			int val = 0;
+
+			if(x > 0 && x + 1 < X)
+				val += abs((int)I.at<uchar>(y,x + 1) - (int)I.at<uchar>(y,x - 1));
+			else if(x > 0)
+				val += 2 * abs((int)I.at<uchar>(y,x) - (int)I.at<uchar>(y,x - 1));
+			else
+				val += 2 * abs((int)I.at<uchar>(y,x + 1) - (int)I.at<uchar>(y,x));
+
+			if(y > 0 && y + 1 < Y)
+				val += abs((int)I.at<uchar>(y + 1,x) - (int)I.at<uchar>(y - 1,x));
+			else if(y > 0)
+				val += 2 * abs((int)I.at<uchar>(y,x) - (int)I.at<uchar>(y - 1,x));
+			else
+				val += 2 * abs((int)I.at<uchar>(y + 1,x) - (int)I.at<uchar>(y,x));
+
+			energy.at<int>(y,x) = val;
+		}
+	}
+
+	return energy;
+}
+
+int main(){
+	Mat_<Vec3b> I = imread("../bench.jpg");
+
+	imshow("seam-carving",I);
+	waitKey(0);
+
+	int Y = I.rows,X = I.cols;
+
+	Mat gray;
+	cvtColor(I,gray,CV_BGR2GRAY);
+
+	Mat energy = calculate_energy(gray);
+
+	imshow("seam-carving",energy);
+	waitKey(0);
+
+	unsigned int dpH[X][Y],dpV[X][Y];
+	int dirH[X][Y],dirV[X][Y];
+	
+	// Horizontal seams
+
+	for(int y = 0;y < Y;++y)
+		dpH[0][y] = energy.at<uint>(y,0);
+
+	for(int x = 1;x < X;++x){
+		for(int y = 0;y < Y;++y){
+			uint val = energy.at<uint>(y,x);
+			dpH[x][y] = -1;
+
+			if(y > 0 && (dpH[x][y] == -1 || val + dpH[x - 1][y - 1] < dpH[x][y])){
+				dpH[x][y] = val + dpH[x - 1][y - 1];
+				dirH[x][y] = -1;
+			}
+
+			if(dpH[x][y] == -1 || val + dpH[x - 1][y] < dpH[x][y]){
+				dpH[x][y] = val + dpH[x - 1][y];
+				dirH[x][y] = 0;
+			}
+
+			if(y + 1 < Y && (dpH[x][y] == -1 || val + dpH[x - 1][y + 1] < dpH[x][y])){
+				dpH[x][y] = val + dpH[x - 1][y + 1];
+				dirH[x][y] = 1;
+			}
+		}
+	}
+
+	Mat seams = I.clone();
+	unsigned int bestH = dpH[X - 1][0];
+	int cury = 0;
+
+	for(int y = 0;y < Y;++y){
+		if(dpH[X - 1][y] < bestH){
+			bestH = dpH[X - 1][y];
+			cury = y;
+		}
+	}
+
+	for(int x = X - 1;x >= 0;--x){
+		seams.at<Vec3b>(cury,x) = Vec3b(0,0,255);
+
+		if(x > 0)
+			cury = cury + dirH[x][cury];
+	}
+
+	// Vertical seams
+
+	for(int x = 0;x < X;++x)
+		dpV[x][0] = energy.at<uint>(0,x);
+
+	for(int y = 1;y < Y;++y){
+		for(int x = 0;x < X;++x){
+			uint val = energy.at<uint>(y,x);
+			dpV[x][y] = -1;
+
+			if(x > 0 && (dpV[x][y] == -1 || val + dpV[x - 1][y - 1] < dpV[x][y])){
+				dpV[x][y] = val + dpV[x - 1][y - 1];
+				dirV[x][y] = -1;
+			}
+
+			if(dpV[x][y] == -1 || val + dpV[x][y - 1] < dpV[x][y]){
+				dpV[x][y] = val + dpV[x][y - 1];
+				dirV[x][y] = 0;
+			}
+
+			if(x + 1 < X && (dpV[x][y] == -1 || val + dpV[x + 1][y - 1] < dpV[x][y])){
+				dpV[x][y] = val + dpV[x + 1][y - 1];
+				dirV[x][y] = 1;
+			}
+		}
+	}
+
+	Mat IV = I.clone();
+	int bestV = dpV[0][Y - 1];
+	int curx = 0;
+
+	for(int x = 0;x < X;++x){
+		if(dpV[x][Y - 1] < bestV){
+			bestV = dpV[x][Y - 1];
+			curx = x;
+		}
+	}
+
+	for(int y = Y - 1;y >= 0;--y){
+		seams.at<Vec3b>(y,curx) = Vec3b(0,0,255);
+
+		if(y > 0)
+			curx = curx + dirV[curx][y];
+	}
+
+	imshow("seam-carving",seams);
+	waitKey(0);
+
+	return 0;
+}

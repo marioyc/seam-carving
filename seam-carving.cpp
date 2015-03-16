cb4c4e2d198a3cbe0ea47c9676b04d53a4665c9b
marioyc <ycmario@gmail.com>
2015-03-16 15:34:15 +0100

    increase size

--- seam-carving.cpp
+++ seam-carving.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include <iostream>
+#include <fstream>
 #include <cstring>
 
 using namespace cv;
@@ -334,11 +335,234 @@ void remove_vertical(Mat &I, int XF){
     }
 }
 
+Vec3b average(Vec3b x, Vec3b y){
+    Vec3b ret;
+
+    for(int i = 0;i < 3;++i)
+        ret.val[i] = (x.val[i] + y.val[i]) / 2;
+    
+    return ret;
+}
+
+void add_horizontal(Mat &I, int YF){
+    Mat I0 = I;
+    int Y0 = I.rows;
+    int Y = Y0,X = I.cols;
+    bool mark[Y][X];
+    int pos[X][Y];
+
+    memset(mark,false,sizeof mark);
+
+    for(int i = 0;i < X;++i)
+        for(int j = 0;j < Y;++j)
+            pos[i][j] = j;
+
+    for(int it = 0;it < YF - Y0;++it){
+        cvtColor(I,gray,CV_BGR2GRAY);
+        energy = calculate_energy(gray);
+
+        for(int y = 0;y < Y;++y)
+            dpH[0][y] = energy.at<int>(y,0);
+
+        for(int x = 1;x < X;++x){
+            for(int y = 0;y < Y;++y){
+                uint val = energy.at<int>(y,x);
+                dpH[x][y] = -1;
+
+                if(y > 0 && (dpH[x][y] == -1 || val + dpH[x - 1][y - 1] < dpH[x][y])){
+                    dpH[x][y] = val + dpH[x - 1][y - 1];
+                    dirH[x][y] = -1;
+                }
+
+                if(dpH[x][y] == -1 || val + dpH[x - 1][y] < dpH[x][y]){
+                    dpH[x][y] = val + dpH[x - 1][y];
+                    dirH[x][y] = 0;
+                }
+
+                if(y + 1 < Y && (dpH[x][y] == -1 || val + dpH[x - 1][y + 1] < dpH[x][y])){
+                    dpH[x][y] = val + dpH[x - 1][y + 1];
+                    dirH[x][y] = 1;
+                }
+            }
+        }
+
+        int bestH = dpH[X - 1][0];
+        int cury = 0;
+
+        for(int y = 0;y < Y;++y){
+            if(dpH[X - 1][y] < bestH){
+                bestH = dpH[X - 1][y];
+                cury = y;
+            }
+        }
+
+        Mat_<Vec3b> tmp(Y - 1,X);
+
+        for(int x = X - 1,cont = 0;x >= 0;--x,++cont){
+            for(int i = 0;i < Y;++i){
+                if(i < cury){
+                    tmp.at<Vec3b>(i,x) = I.at<Vec3b>(i,x);
+                }else if(i > cury){
+                    tmp.at<Vec3b>(i - 1,x) = I.at<Vec3b>(i,x);
+                    pos[x][i - 1] = pos[x][i];
+                }else{
+                    mark[ pos[x][i] ][x] = true;
+                }
+            }
+
+            if(x > 0)
+                cury = cury + dirH[x][cury];
+        }
+
+        I = tmp;
+        --Y;
+    }
+
+    Mat_<Vec3b> tmp(YF,X);
+
+    for(int j = 0;j < X;++j){
+        int cont = 0;
+
+        for(int i = 0;i < Y0;++i){
+            if(mark[i][j]){
+                Vec3b aux;
+
+                if(i == 0) aux = average(I0.at<Vec3b>(i,j),I0.at<Vec3b>(i + 1,j));
+                else if(i == Y0 - 1) aux = average(I0.at<Vec3b>(i,j),I0.at<Vec3b>(i - 1,j));
+                else aux = average(I0.at<Vec3b>(i - 1,j),I0.at<Vec3b>(i + 1,j));
+
+                tmp.at<Vec3b>(cont,j) = aux; cont++;
+                tmp.at<Vec3b>(cont,j) = aux; cont++;
+            }else{
+                tmp.at<Vec3b>(cont,j) = I0.at<Vec3b>(i,j);
+                cont++;
+            }
+        }
+    }
+
+    I = tmp;
+}
+
+void add_vertical(Mat &I, int XF){
+    Mat I0 = I;
+    int X0 = I.cols;
+    int X = X0,Y = I.rows;
+    bool mark[Y][X];
+    int pos[X][Y];
+
+    memset(mark,false,sizeof mark);
+
+    for(int i = 0;i < X;++i)
+        for(int j = 0;j < Y;++j)
+            pos[i][j] = i;
+
+    for(int it = 0;it < XF - X0;++it){
+        cvtColor(I,gray,CV_BGR2GRAY);
+        energy = calculate_energy(gray);
+
+        for(int x = 0;x < X;++x)
+            dpV[x][0] = energy.at<int>(0,x);
+
+        for(int y = 1;y < Y;++y){
+            for(int x = 0;x < X;++x){
+                int val = energy.at<int>(y,x);
+                dpV[x][y] = -1;
+
+                if(x > 0 && (dpV[x][y] == -1 || val + dpV[x - 1][y - 1] < dpV[x][y])){
+                    dpV[x][y] = val + dpV[x - 1][y - 1];
+                    dirV[x][y] = -1;
+                }
+
+                if(dpV[x][y] == -1 || val + dpV[x][y - 1] < dpV[x][y]){
+                    dpV[x][y] = val + dpV[x][y - 1];
+                    dirV[x][y] = 0;
+                }
+
+                if(x + 1 < X && (dpV[x][y] == -1 || val + dpV[x + 1][y - 1] < dpV[x][y])){
+                    dpV[x][y] = val + dpV[x + 1][y - 1];
+                    dirV[x][y] = 1;
+                }
+            }
+        }
+
+        int bestV = dpV[0][Y - 1];
+        int curx = 0;
+
+        for(int x = 0;x < X;++x){
+            if(dpV[x][Y - 1] < bestV){
+                bestV = dpV[x][Y - 1];
+                curx = x;
+            }
+        }
+
+        Mat_<Vec3b> tmp(Y,X - 1);
+
+        for(int y = Y - 1;y >= 0;--y){
+            //if(it == 1) cout << y << endl;
+            for(int i = 0;i < X;++i){
+                if(i < curx){
+                    tmp.at<Vec3b>(y,i) = I.at<Vec3b>(y,i);
+                }else if(i > curx){
+                    tmp.at<Vec3b>(y,i - 1) = I.at<Vec3b>(y,i);
+                    pos[i - 1][y] = pos[i][y];
+                }else{
+                    mark[y][ pos[i][y] ] = true;
+                }
+            }
+
+            if(y > 0)
+                curx = curx + dirV[curx][y];
+        }
+        I = tmp;
+        --X;
+    }
+
+    Mat_<Vec3b> tmp(Y,XF);
+
+    for(int i = 0;i < Y;++i){
+        int cont = 0;
+
+        for(int j = 0;j < X0;++j){
+            if(mark[i][j]){
+                Vec3b aux;
+
+                if(j == 0) aux = average(I0.at<Vec3b>(i,j),I0.at<Vec3b>(i,j + 1));
+                else if(j == X0 - 1) aux = average(I0.at<Vec3b>(i,j),I0.at<Vec3b>(i,j - 1));
+                else aux = average(I0.at<Vec3b>(i,j - 1),I0.at<Vec3b>(i,j + 1));
+
+                tmp.at<Vec3b>(i,cont) = aux; cont++;
+                tmp.at<Vec3b>(i,cont) = aux; cont++;
+            }else{
+                tmp.at<Vec3b>(i,cont) = I0.at<Vec3b>(i,j);
+                cont++;
+            }
+        }
+    }
+
+    I = tmp;
+}
+
+void process(Mat &I, int YF, int XF){
+    cout << "Process (" << I.rows << ", " << I.cols << ") -> (" << YF << ", " << XF << ")" << endl;
+    if(YF < I.rows)
+        remove_horizontal(I,YF);
+    else if(YF > I.rows)
+        add_horizontal(I,YF);
+
+    if(XF < I.cols)
+        remove_vertical(I,XF);
+    else if(XF > I.cols)
+        add_vertical(I,XF);
+}
+
 int main(){
-    Mat_<Vec3b> I = imread("../bench.jpg");
-    //Mat_<Vec3b> I = imread("../christmas.jpg");
-    //Mat_<Vec3b> I = imread("../dolphin.jpg");
-    //Mat_<Vec3b> I = imread("../beach.jpg");
+    freopen("../input.txt","r",stdin);
+
+    string file;
+    cin >> file;
+    cout << file << endl;
+
+    Mat_<Vec3b> I = imread(file);
 
     imshow("seam-carving",I);
     waitKey(0);
@@ -351,18 +575,22 @@ int main(){
 
     cout << "Rows = ";
     cin >> YF;
-
-    remove_horizontal(I,YF);
+    YF += Y0;
 
     cout << "Cols = ";
     cin >> XF;
+    XF += X0 ;
 
-    remove_vertical(I,XF);
-
-    //reduce(I,YF,XF);
+    process(I,YF,XF);
 
     imshow("seam-carving-out",I);
     waitKey(0);
+
+    process(I,Y0,X0);
+    imshow("seam-carving-out-2",I);
+    waitKey(0);
+
+    fclose(stdin);
 
     return 0;
 }
